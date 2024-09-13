1ebce3f9961225fe5ce35adb5ffd4c83478c36b9
RASHWANTH <111727055+rashwanth17@users.noreply.github.com>
2024-09-13 09:41:15 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,12 @@
+const name=document.getElementById("name");
+const email=document.getElementById("email");
+const phn=document.getElementById("phn");
+const result = document.getElementById("result");
+const submitButton = document.getElementById("submit");
+
+submit.onclick=function(){ result.textContent="hello "+name.value;}
+
+
+
+
+
